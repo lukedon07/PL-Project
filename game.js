60dc641f76d513c2153348c6f430c06e130fd540
Luke Donaldson <ldonaldson070@gmail.com>
2025-11-22 15:46:40 -0700

    added a button, should pop up text on click

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 function myFirstFunction(){
-    let aString = "Hello World from JavaScript";
+    let aString = "This function should be called once the button has been clicked";
     document.body.innerHTML += "<p>" + aString + "</p>";
 }
 
-myFirstFunction();
+const button = document.createElement("myButton");
+button.innertext = "Click This!";
+
+button.addEventListener("click", myFirstFunction);
+document.body.appendChild(button);
