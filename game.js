961d27069f5fd35cc7a011e2067e9d093873731d
Luke Donaldson <ldonaldson070@gmail.com>
2025-11-23 12:14:42 -0700

    game window should now appear upon click

--- game.js
+++ game.js
@@ -1,20 +1,18 @@
-function myFirstFunction() {
+function createGame() {
     let aString = "This function should be called once the button has been clicked";
     document.body.innerHTML += "<p>" + aString + "</p>";
 }
 
-function button() {
+function initializeButtons() {
     const openButton = document.getElementById("openGameButton");
     const closeButton = document.getElementById("closeGameButton");
     const gameWindow = document.getElementById("gameWindow");
 
     openButton.innerText = "Click This To Play A Game!";
 
-    document.body.appendChild(button);
-
     openButton.addEventListener("click", () => {
         gameWindow.style.display = "block";
-        myFirstFunction();
+        createGame();
     });
 
     closeButton.addEventListener("click", () => {
@@ -22,7 +20,7 @@ function button() {
     })
 }
 
-button();
+document.addEventListener("DOMContentLoaded", initializeButtons);
 
 
 
