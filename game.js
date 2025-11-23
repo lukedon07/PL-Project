42d08167aa6d0d330ac02d7ff7aa51886d01f767
Luke Donaldson <ldonaldson070@gmail.com>
2025-11-22 18:38:38 -0700

    pleeeaseesa mr button

--- game.js
+++ game.js
@@ -1,20 +1,21 @@
-function myFirstFunction(){
+function myFirstFunction() {
     let aString = "This function should be called once the button has been clicked";
     document.body.innerHTML += "<p>" + aString + "</p>";
 }
 
-function button(){
-    const button = document.createElement("myButton");
-    button.innertext = "Click This!";
+function button() {
+    const button = document.createElement("button");
+    button.innerText = "Click This!";
+
     document.body.appendChild(button);
 
     button.addEventListener("click", () => {
-        alert("button clicked!");
+        alert("BUTTON HAS BEEN CLICKED");
+        myFirstFunction();
     });
-
 }
 
-button()
+button();
 
 
 
